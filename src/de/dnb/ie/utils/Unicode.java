a2fed0ef311936fa76e26c54fa108cf9eeffb69b
Christian Baumann <k.c.baumann@t-online.de>
2025-02-18 18:13:00 +0100

    Hilfsfunktion für Unicode-zerlegte

---
+++ src/de/dnb/ie/utils/Unicode.java
@@ -0,0 +1,17 @@
+package de.dnb.ie.utils;
+
+import de.dnb.basics.applicationComponents.strings.StringUtils;
+
+/**
+ * Macht aus der Zwischenablage eine mittels Editor suchbare (zerlegte Umlaute werden
+ * zusammengefügt).
+ */
+public class Unicode {
+
+	public static void main(String[] args) {
+		String s = StringUtils.readClipboard();
+		StringUtils.writeToClipboard(StringUtils.unicodeComposition(s));
+
+	}
+
+}
