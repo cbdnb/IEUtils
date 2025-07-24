a2d2ef3a93ce47054c1945b2a78bd2d870c07141
Christian Baumann <k.c.baumann@t-online.de>
2025-07-24 22:56:19 +0200

    Multitool für 4700 und 5051

--- src/de/dnb/ie/automatic/Gesamtbewertung.java
+++ src/de/dnb/ie/automatic/Gesamtbewertung.java
@@ -27,6 +27,7 @@ import de.dnb.gnd.parser.line.LineFactory;
 import de.dnb.gnd.parser.tag.BibTagDB;
 import de.dnb.gnd.parser.tag.Tag;
 import de.dnb.gnd.utils.RecordUtils;
+import java.awt.Font;
 
 /**
  * @author baumann
@@ -86,45 +87,56 @@ public class Gesamtbewertung {
 				exit();
 			}
 		});
-		frmGesamtbewertung
-				.setTitle("Ausschluss dieses Dokuments aus dem QM-Prozess");
-		frmGesamtbewertung.setIconImage(Toolkit.getDefaultToolkit()
-				.getImage(Gesamtbewertung.class.getResource(
-						"/com/sun/javafx/scene/control/skin/caspian/dialog-information.png")));
-		frmGesamtbewertung.setBounds(100, 100, 541, 331);
+		frmGesamtbewertung.setTitle("Ausschluss dieses Dokuments aus dem QM-Prozess / Kommentar / Kein Culturegraph");
+		frmGesamtbewertung.setBounds(100, 100, 947, 350);
 		frmGesamtbewertung.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmGesamtbewertung.getContentPane().setLayout(null);
 
 		textFieldComment = new JTextField();
-		textFieldComment.setBounds(25, 75, 410, 20);
+		textFieldComment.setBounds(24, 40, 460, 20);
 		frmGesamtbewertung.getContentPane().add(textFieldComment);
 		textFieldComment.setColumns(10);
 
-		final JLabel lblKommentarfakultativ = new JLabel(
-				"Kommentar (optional, aber hilfreich):");
+		final JLabel lblKommentarfakultativ = new JLabel("Kommentar in 4700:");
 		lblKommentarfakultativ.setHorizontalAlignment(SwingConstants.LEFT);
 		lblKommentarfakultativ.setLabelFor(textFieldComment);
-		lblKommentarfakultativ.setBounds(25, 44, 221, 20);
+		lblKommentarfakultativ.setBounds(25, 21, 221, 20);
 		frmGesamtbewertung.getContentPane().add(lblKommentarfakultativ);
 
 		final JButton btnAbbruch = new JButton("Abbruch");
+		btnAbbruch.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnAbbruch.addActionListener(e -> exit());
-		btnAbbruch.setBounds(349, 185, 89, 23);
+		btnAbbruch.setBounds(253, 234, 133, 35);
 		frmGesamtbewertung.getContentPane().add(btnAbbruch);
 
 		btnKeineSammlung = new JButton("Kein Sammelgebiet (KNSG)");
-		btnKeineSammlung.addActionListener(a -> ksg());
-		btnKeineSammlung.setBounds(25, 172, 216, 23);
+		btnKeineSammlung.addActionListener(a -> knsg());
+		btnKeineSammlung.setBounds(24, 180, 216, 23);
 		frmGesamtbewertung.getContentPane().add(btnKeineSammlung);
 
 		btnKeineErschliessung = new JButton("Keine Erschließung (KNIE)");
-		btnKeineErschliessung.addActionListener(a -> kie());
-		btnKeineErschliessung.setBounds(25, 206, 216, 23);
+		btnKeineErschliessung.addActionListener(a -> knie());
+		btnKeineErschliessung.setBounds(268, 180, 216, 23);
 		frmGesamtbewertung.getContentPane().add(btnKeineErschliessung);
 
-		lblNewLabel = new JLabel("Grund für den Ausschluss aus QM:");
-		lblNewLabel.setBounds(25, 131, 221, 20);
+		lblNewLabel = new JLabel("Grund für den Ausschluss aus QM (Eventuelle Kommentare werden übernommen):");
+		lblNewLabel.setBounds(25, 160, 444, 20);
 		frmGesamtbewertung.getContentPane().add(lblNewLabel);
+
+		JButton btnNurKommentar = new JButton("Nur Kommentar einfügen");
+		btnNurKommentar.addActionListener(e -> writeOnlyComm("QM*" + getKommentar()));
+		btnNurKommentar.setBounds(497, 40, 200, 20);
+		frmGesamtbewertung.getContentPane().add(btnNurKommentar);
+
+		JButton btnKeinebernahmeAus = new JButton("Keine Übernahme aus Culturegraph");
+		btnKeinebernahmeAus.addActionListener(e -> writeOnlyComm("$gcg-ie-null"));
+		btnKeinebernahmeAus.setBounds(24, 106, 244, 23);
+		frmGesamtbewertung.getContentPane().add(btnKeinebernahmeAus);
+
+		JLabel lblMitEinemEintrag = new JLabel("Mit einem Eintrag in 4700 die Übernahme aus Culturegraph verhindern:");
+		lblMitEinemEintrag.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMitEinemEintrag.setBounds(25, 82, 419, 20);
+		frmGesamtbewertung.getContentPane().add(lblMitEinemEintrag);
 	}
 
 	/**
@@ -142,16 +154,15 @@ public class Gesamtbewertung {
 		System.exit(0);
 	}
 
-	private void ksg() {
+	private void knsg() {
 		ausschlussAusQM("KNSG");
 	}
 
-	private void kie() {
+	private void knie() {
 		ausschlussAusQM("KNIE");
 	}
 
 	private void ausschlussAusQM(final String grund) {
-		final String comm = getKommentar();
 
 		final Line line5051 = RecordUtils.getTheOnlyLine(record, "5051");
 		if (line5051 == null) {
@@ -161,8 +172,7 @@ public class Gesamtbewertung {
 		final List<Subfield> subs = line5051.getSubfields();
 		final Tag tag = line5051.getTag();
 		final Indicator indicator = tag.getIndicator('a');
-		subs.removeIf(sub ->
-		{
+		subs.removeIf(sub -> {
 			return sub.getIndicator() == indicator;
 		});
 
@@ -182,12 +192,22 @@ public class Gesamtbewertung {
 		final Line newLine = fac.createLine();
 		String out = RecordUtils.toPica(newLine, Format.PICA3, false, '$');
 
+		final String comm = getKommentar();
+
 		if (!StringUtils.isNullOrEmpty(comm))
 			out += "\n" + "4700 |IE|QM*" + comm;
 
 		StringUtils.writeToClipboard(out);
 		System.err.println(out);
 		System.exit(0);
+	}
 
+	private void writeOnlyComm(String comm) {
+		if (!StringUtils.isNullOrEmpty(comm)) {
+			String out = "4700 |IE|" + comm;
+			StringUtils.writeToClipboard(out);
+			System.err.println(out);
+			System.exit(0);
+		}
 	}
 }
